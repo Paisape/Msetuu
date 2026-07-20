f8920e5392cb6549f14e0b695fbb11dbc5973e59
Developer <dev@mandirsetuu.com>
2026-07-20 18:57:57 +0530

    feat: add openssl/libc6-compat to docker runner, set sh script exit fail flag

--- Web/entrypoint.sh
+++ Web/entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
+set -e
 echo "Running database migrations..."
-npx prisma db push --schema=./src/prisma/schema.prisma --accept-data-loss
+prisma db push --schema=./src/prisma/schema.prisma --accept-data-loss
 echo "Running database seeding..."
-npx prisma db seed
+prisma db seed
 echo "Starting Next.js server..."
 exec node server.js
